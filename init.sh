bc98d5915e9cdc664d9f49d1ec7587e084bd2a03
matandomuertos <nahuel_cassinari@hotmail.com>
2023-05-03 16:37:41 +0200

    Added script

---
+++ init.sh
@@ -0,0 +1,19 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask google-chrome
+brew install --cask steam
+brew install --cask visual-studio-code
+brew install --cask discord
+brew install --cask coconutbattery
+brew install --cask google-drive
+brew install --cask topnotch
+brew install --cask rectangle
+brew install --cask spotify
+brew install --cask tunnelbear
+defaults write com.apple.dock persistent-apps -array && killall Dock
+brew install bash && sudo chsh -s /usr/local/bin/bash $(whoami)
+brew install git
+brew install wget
+brew install unzip
+brew install telnet
+sudo gem install iStats
+sudo echo "192.168.31.167 koko" >> /etc/hosts
