ccaee49c993c08313f81ba76475d4276085aae9a
matandomuertos <nahuel_cassinari@hotmail.com>
2023-05-04 18:57:45 +0200

    added some cofig

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
+# Install stuff
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
 brew install --cask google-chrome
 brew install --cask steam
 brew install --cask visual-studio-code
@@ -9,11 +11,19 @@ brew install --cask topnotch
 brew install --cask rectangle
 brew install --cask spotify
 brew install --cask tunnelbear
-defaults write com.apple.dock persistent-apps -array && killall Dock
 brew install bash && sudo chsh -s /usr/local/bin/bash $(whoami)
 brew install git
 brew install wget
 brew install unzip
 brew install telnet
 sudo gem install iStats
+
+# Add koko to hosts
 sudo echo "192.168.31.167 koko" >> /etc/hosts
+
+## Configure MacOs ##
+# Remove all the icons from the dock
+defaults write com.apple.dock persistent-apps -array && killall Dock
+# Ajust mouse sensibility
+defaults write .GlobalPreferences com.apple.mouse.scaling 0.6875
+
